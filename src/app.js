e1d4c3f1b72830d1ca57d86681f721a1123c83b9
Danilo Augusto <danilloagt@gmail.com>
2023-06-18 13:17:12 -0300

    update

--- src/app.js
+++ src/app.js
@@ -14,9 +14,8 @@ app.use(morgan('common'))
 
 
 // variaveis da API
-const END_POINT_V2 = process.env.END_POINT_V2 //recupera end point v2 do arquivo .env 
+const END_POINT_V2 = process.env.END_POINT_V2 //recupera end point v2 do arquivo .env
 const routerAPI2 = require('./routes/routerAPI2') // arquivos de rotas V1
 app.use(END_POINT_V2, routerAPI2)
-
 
 module.exports = app;
