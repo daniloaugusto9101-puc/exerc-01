f9fbce431b7f9f9b34b696660cde892864d58aac
Danilo Augusto <danilloagt@gmail.com>
2023-06-18 14:36:54 -0300

    update

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-// const END_POINT_V2 = process.env.END_POINT_V2 //recupera end point v2 do arquivo .env
-const routerAPI2 = require('./routes/routerAPI2') // arquivos de rotas V1
+require('dotenv').config(); // para utilizar variaveis de ambiente
 
-// para utilizar variaveis de ambiente
-require('dotenv').config();
+const routerAPI2 = require('./routes/routerAPI2') // arquivos de rotas V2
+const END_POINT_V2 = process.env.END_POINT_V2 //recupera end point v2 do arquivo .env 
+
 
 //aceita corpo json
 app.use(express.json())
@@ -15,6 +15,6 @@ app.use(express.json())
 app.use(morgan('common'))
 
 // variaveis da API
-app.use('/api/v2', routerAPI2)
+app.use(END_POINT_V2, routerAPI2)
 
 module.exports = app;
