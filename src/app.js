c2b18f02f7411a3d6d10fdd325d28776b5c469bf
Danilo Augusto <danilloagt@gmail.com>
2023-06-18 12:18:59 -0300

    update

--- src/app.js
+++ src/app.js
@@ -10,12 +10,12 @@ app.use(router);
 
 
 // variaveis da API
-const END_POINT_V1 = process.env.END_POINT_V1 //recupera end point v2 do arquivo .env 
+// const END_POINT_V1 = process.env.END_POINT_V1 //recupera end point v2 do arquivo .env 
 const END_POINT_V2 = process.env.END_POINT_V2 //recupera end point v2 do arquivo .env 
 
 // Dados de acesso apiV1
-const routerAPI1 = require('./routes/routerAPI1') // arquivos de rotas V1
-app.use(END_POINT_V1, routerAPI1)
+// const routerAPI1 = require('./routes/routerAPI1') // arquivos de rotas V1
+// app.use(END_POINT_V1, routerAPI1)
 
 // Dados de acesso apiV2
 const routerAPI2 = require('./routes/routerAPI2') // arquivos de rotas V1
--- src/server.js
+++ src/server.js
@@ -7,14 +7,14 @@ require('dotenv').config() //da acesso as variaveis de ambiente dentro do arquiv
 
 // variaveis da API
 const PORT = process.env.PORT || 3333 //recupera porta do arquivo .env 
-const END_POINT_V1 = process.env.END_POINT_V1 //recupera end point v2 do arquivo .env 
+// const END_POINT_V1 = process.env.END_POINT_V1 //recupera end point v2 do arquivo .env 
 const END_POINT_V2 = process.env.END_POINT_V2 //recupera end point v2 do arquivo .env 
 
 
-app.use('/', (req, res) => {
-    res.status(200)
-    res.send(`Bem vindo a minha API, acesse: \n ${END_POINT_V1}/produtos`)
-})
+// app.use('/', (req, res) => {
+//     res.status(200)
+//     res.send(`Bem vindo a minha API, acesse: \n ${END_POINT_V1}/produtos`)
+// })
 
 app.use('/', (req, res) => {
     res.status(200)
