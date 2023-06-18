5f789dec3da3474b28405c7a662347b2b4f4acc7
Danilo Augusto <danilloagt@gmail.com>
2023-06-18 13:11:21 -0300

    bkp arquivos

--- src/app.js
+++ src/app.js
@@ -1,23 +1,20 @@
-const express = require('express') //Framework rotas e servidor
+//Framework rotas e servidor
+const express = require('express')
 const app = express()
-const router = require('./routes/routerAPI2'); // rotas da API V2
 
-const morgan = require('morgan') // para criação de logs
+// para utilizar variaveis de ambiente
+require('dotenv').config();
 
-app.use(express.json()) //aceita corpo json
-app.use(morgan('common')) //criar log da API
-app.use(router);
+//aceita corpo json
+app.use(express.json())
+
+// Para criação de logs
+const morgan = require('morgan')
+app.use(morgan('common'))
 
 
 // variaveis da API
-// const END_POINT_V1 = process.env.END_POINT_V1 //recupera end point v2 do arquivo .env 
 const END_POINT_V2 = process.env.END_POINT_V2 //recupera end point v2 do arquivo .env 
-
-// Dados de acesso apiV1
-// const routerAPI1 = require('./routes/routerAPI1') // arquivos de rotas V1
-// app.use(END_POINT_V1, routerAPI1)
-
-// Dados de acesso apiV2
 const routerAPI2 = require('./routes/routerAPI2') // arquivos de rotas V1
 app.use(END_POINT_V2, routerAPI2)
 
